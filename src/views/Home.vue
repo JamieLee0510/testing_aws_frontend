02b4bb5884b8152319c9818f25b3a52410b4bec4
ding <walker200665@gmail.com>
2021-03-18 23:36:32 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>{{ message }}</h1>
+    <button @click="testing">testing</button>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 export default {
-  name: 'Home',
+  name: "Home",
+  data() {
+    return {
+      message: "",
+      API_URL:
+        "http://testnode-env.eba-s5cqe8ci.us-east-2.elasticbeanstalk.com/api/testing",
+    };
+  },
+  methods: {
+    async testing() {
+      await axios.get(this.API_URL).then((res) => {
+        this.message = res.data.message;
+      });
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
